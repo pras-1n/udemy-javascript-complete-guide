968eb1930d0c95906bb103f713ef6b117cfe3d1e
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-06 19:15:02 +0545

    adding a reusable function

--- 02-basics-calculator/assets/scripts/script.js
+++ 02-basics-calculator/assets/scripts/script.js
@@ -12,27 +12,38 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription); //from vendor file
 }
+
+function writeToLog(
+    operationIdentifier, 
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry.operation);
+    console.log(logEntries);  
+}
   
 function add(){ 
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'ADD',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);  
-} 
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
 
 function subtract(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+
 }
 
 function multiply(){
@@ -40,7 +51,7 @@ function multiply(){
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
-
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide(){
@@ -48,7 +59,7 @@ function divide(){
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
     createAndWriteOutput('÷', initialResult, enteredNumber);
-
+    writeToLog('DIVISION', initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
@@ -56,3 +67,4 @@ subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide) ;
 
+//VSCODE GIT
