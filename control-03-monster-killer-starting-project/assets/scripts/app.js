6dc777046a64d89b5e238c46d3cd911f959a866a
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-09 22:10:49 +0545

    added if/else line for win/lose mechanic

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,13 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
 	const damage = dealMonsterDamage(ATTACK_VALUE);
 	currentMonsterHealth -= damage;
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+	currentPlayerHealth -= playerDamage;
+	if (currentMonsterHealth <= 0) {
+		alert("You won.");
+	} else if (currentPlayerHealth <= 0) {
+		alert("You lost.");
+	}
 }
 
 attackBtn.addEventListener("click", attackHandler);
