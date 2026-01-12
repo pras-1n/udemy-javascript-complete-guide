1660259d9fec51340af9984398c11e700e90a5cd
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-12 16:32:28 +0545

    added reset game mechanic

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,12 +3,25 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt("Maximum life for you and the monster.", "100");
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+	chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function reset() {
+	currentMonsterHealth = chosenMaxLife;
+	currentPlayerHealth = chosenMaxLife;
+	resetGame(chosenMaxLife);
+}
 
 function endRound() {
 	const initialPlayerHealth = currentPlayerHealth;
@@ -19,8 +32,8 @@ function endRound() {
 		hasBonusLife = false;
 		removeBonusLife();
 		currentPlayerHealth = initialPlayerHealth;
-		alert("Used bonus health.");
 		setPlayerHealth(initialPlayerHealth);
+		alert("Used bonus health.");
 	}
 
 	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -29,6 +42,10 @@ function endRound() {
 		alert("You lost.");
 	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
 		alert("You have a draw.");
+	}
+
+	if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+		reset();
 	}
 }
 
