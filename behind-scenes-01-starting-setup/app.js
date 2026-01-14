55533e9ffa82e6fddf4645df330598c5e121ad3e
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-14 13:06:04 +0545

    set up new module

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+let name = "Prashant";
+
+if (name === "Prashant") {
+	let hobbies = ["Language learning", "Reading philosophies"];
+	console.log(hobbies);
+}
+
+function greet() {
+	let age = 30;
+	let name = "Johan";
+	console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
