cc6f158878219107c14ad2b2d1d07fc53d551b7a
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-14 19:52:15 +0545

    misc

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,16 +1,57 @@
-let name = "Prashant";
+// let name = "Prashant";
 
-if (name === "Prashant") {
-	let hobbies = ["Language learning", "Reading philosophies"];
-	console.log(hobbies);
+// if (name === "Prashant") {
+// 	let hobbies = ["Language learning", "Reading philosophies"];
+// 	console.log(hobbies);
+// }
+
+// function greet() {
+// 	let age = 30;
+// 	let name = "Johan";
+// 	console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+//
+
+// 'use strict';
+
+// const username = "Moisty";
+// var userName = "Johan";
+
+// console.log(userName);
+
+// function getName() {
+// 	return prompt("Your name: ", "");
+// }
+
+// function greet() {
+// 	const userName = getName();
+// 	console.log("Hello" + userName);
+// }
+
+// greet();
+
+const addListenerBtn = document.getElementById('add-listener-btn');
+const addListenerBtn = document.getElementById('clickable-btn');
+const addListenerBtn = document.getElementById('click-message-input');
+
+let person = {name: 'Pras'};
+
+person = null;
+
+function printMessage() {
+	const value = messageInput.value;
+	console.log(value || 'Clicked me!');
 }
 
-function greet() {
-	let age = 30;
-	let name = "Johan";
-	console.log(name, age, hobbies);
+funtion  addListener () {
+	clickableBtn.addEventListener('click', function(){
+		const value = messageInput.value;
+		console.log(value || 'Clicked me!');
+	});
 }
 
-console.log(name, hobbies);
-
-greet();
+addListenerBtn.addEventListener('click', addListener);
