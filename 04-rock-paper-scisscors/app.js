23fcedd3145b0ccd12b55b45f287444ad4a95045
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-16 14:36:55 +0545

    added default arguments

--- 04-rock-paper-scisscors/app.js
+++ 04-rock-paper-scisscors/app.js
@@ -4,20 +4,27 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = "ROCK";
+
 const RESULT_DRAW = "DRAW";
 const RESULT_PLAYER_WINS = "PLAYER_WINS";
 const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
 	const selection = prompt(
-		"${ROCK}, ${PAPER} or ${SCISSORS?",
+		`${ROCK}, ${PAPER} or ${SCISSORS} のどれを出しましか？`,
 		"",
 	).toUpperCase();
+
+	if (selection === null || selection === "") {
+		alert(`無効な値です！　代わりに${DEFAULT_USER_CHOICE}を選択しました。`);
+		return;
+	}
+
 	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-		alert("Invalid choice! We choose ${Rock} for you!");
-		return DEFAULT_USER_CHOICE;
+		alert("無効な値です！　代わりに${Rock}を選択しました。");
+		return;
 	}
 	return selection;
 };
@@ -33,28 +40,49 @@ const getComputerChoice = function () {
 	}
 };
 
-const getWinner = function (cChoice, pChoice) {
-	if (cChoice === pChoice) {
-		return RESULT_DRAW;
-	} else if (
-		(cChoice === ROCK && pChoice === PAPER) ||
-		(cChoice === PAPER && pChoice === SCISSORS) ||
-		(cChoice === SCISSORS && pChoice === ROCK)
-	) {
-		return RESULT_PLAYER_WINS;
-	} else {
-		return RESULT_COMPUTER_WINS;
-	}
-};
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
+	cChoice === pChoice
+		? RESULT_DRAW
+		: (cChoice === ROCK && pChoice === PAPER) ||
+			  (cChoice === PAPER && pChoice === SCISSORS) ||
+			  (cChoice === SCISSORS && pChoice === ROCK)
+			? RESULT_PLAYER_WINS
+			: RESULT_COMPUTER_WINS;
 
-startGameBtn.addEventListener("click", function () {
+// if (cChoice === pChoice) {
+// 	return RESULT_DRAW;
+// } else if (
+// 	(cChoice === ROCK && pChoice === PAPER) ||
+// 	(cChoice === PAPER && pChoice === SCISSORS) ||
+// 	(cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+// 	return RESULT_PLAYER_WINS;
+// } else {
+// 	return RESULT_COMPUTER_WINS;
+// }
+
+startGameBtn.addEventListener("click", () => {
 	if (gameIsRunning) {
 		return;
 	}
 	gameIsRunning = "true";
 	console.log("Game is starting...");
 	const playerChoice = getPlayerChoice();
-	const ComputerChoice = getComputerChoice();
-	const winner = getWinner(ComputerChoice, playerChoice);
-	console.log(winner);
+	const computerChoice = getComputerChoice();
+	let winner;
+	if (playerChoice) {
+		const winner = getWinner(ComputerChoice, playerChoice);
+	} else {
+		winner = getWinner(ComputerChoice);
+	}
+	let message = `You picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+	if (winner === RESULT_DRAW) {
+		message = message + "had a draw.";
+	} else if (winner === RESULT_PLAYER_WINS) {
+		message = message + "won.";
+	} else {
+		message = message + "lost.";
+	}
+	alert(message);
+	gameIsRunning = false;
 });
