c20f921a2f428b39e7a0e5c8a325a87f1c01c1b8
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-19 20:01:45 +0545

    fixing backdrop and cancel button

--- 05-DOM/02-dom-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
+++ 05-DOM/02-dom-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
@@ -3,11 +3,29 @@ const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.body.children(1);
 const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+	backdrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
 	// function () {}
 	// addMovieModal.className = 'modal card'
 	addMovieModal.classList.toggle("visible");
+	// backdrop.classList.toggle("visible");
+	toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+	toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+	toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie;
