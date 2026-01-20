1e62fc7bcbe349de59ae301df3f2cd5266788ab3
Prashant Guragai <prashantguragai1016@gmail.com>
2026-01-20 19:34:39 +0545

    rendering movie items on the screen

--- 05-DOM/02-dom-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
+++ 05-DOM/02-dom-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
@@ -8,8 +8,33 @@ const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 // const userInputs = addMovieModal.getElementByTagName('input');
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUI = () => {
+	if (movies.length === 0) {
+		entryTextSection.style.display = "block";
+	} else {
+		entryTextSection.style.display = "none";
+	}
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+	const newMovieElement = document.createElement("li");
+	newMovieElement.className = "movie-element";
+	newMovieElement.innerHTML = `
+		<div class = "movie-element__image">
+			<img src="${imageUrl}" alt="${title}">
+		</div>
+		<div class = "movie-element_info">
+			<h2>${title}</h2>
+			<p>${rating}/5 stars</p>
+		</div>
+	`;
+	const listRoot = document.getElementById("movie-list");
+	listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
 	backdrop.classList.toggle("visible");
@@ -24,7 +49,7 @@ const toggleMovieModal = () => {
 };
 
 const clearMovieInputs = () => {
-	for (const usrInputs of userInputs) {
+	for (const usrInput of userInputs) {
 		usrInput.value = "";
 	}
 };
@@ -58,8 +83,10 @@ const addMovieHandler = () => {
 
 	movies.push(newMovie);
 	console.log(movies);
-	toggleMovieMo dal();
+	toggleMovieModal();
 	clearMovieInputs();
+	renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+	updateUI();
 };
 
 const backdropClickHandler = () => {
