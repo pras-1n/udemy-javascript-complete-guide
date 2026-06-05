377ef480b55f27d40ba93ac9e4997636ddd5e16c
Prashant Guragai <prashantguragai1016@gmail.com>
2026-06-05 19:15:07 +0545

    which data can you store in arrays

---
+++ 06-more-on-functions/app.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
+
+const hobbies = ['Cooking', 'Sports'];
+const personalData = [30, 'Max', {moreDetails: []}];
