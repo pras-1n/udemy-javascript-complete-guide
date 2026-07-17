cdaa008c44fe74d701b34759bb30c4027ccfd047
Prashant Guragai <prashantguragai1016@gmail.com>
2026-07-17 19:07:39 +0545

    update button ID for movie addition and enhance movie handling logic

--- 09-more-on-objects/scripts/objects.js
+++ 09-more-on-objects/scripts/objects.js
@@ -1,21 +1,60 @@
-const movieList = document.getElementById('movie-list');
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+const movies = [];
 
-let person = {
-  "first name": "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  greet: function () {
-    alert("Hi there!");
-  },
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
 };
 
-// person.age = 31;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-person.isAdmin = true;
+addMovieBtn.addEventListener('click', addMovieHandler);
 
-console.log(person["first name"]);
+// const movieList = document.getElementById("movie-list");
+
+// movieList.style["background-color"] = "red";
+// movieList.style.display = "block";
+
+// const userChosenKeyName = 'level';
+
+// let person = {
+//   "first name": "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   greet: function () {
+//     alert("Hi there!");
+//   },
+//   1.5: "hello",
+// };
+
+// // person.age = 31;
+// delete person.age;
+// // person.age = undefined;
+// // person.age = null;
+// person.isAdmin = true;
+
+// const keyName = 'first name';
+
+// console.log(person[keyName]);
+// console.log(person[1.5]);
+// console.log(person);
